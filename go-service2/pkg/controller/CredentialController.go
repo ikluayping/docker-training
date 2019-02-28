fe78142da46ab4a554a6ca37c6e3b04a79596e1d
ikluayping <thiangtham.k@hotmail.com>
2019-02-28 11:52:45 +0700

    Update project for traning

--- go-service2/pkg/controller/CredentialController.go
+++ go-service2/pkg/controller/CredentialController.go
@@ -4,14 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"go-service2/pkg/model"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
+//Healthz use for checking livness and readiness probe
 func Healthz(w http.ResponseWriter, request *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+//PostLogin is a function for validate Login from request
 func PostLogin(w http.ResponseWriter, request *http.Request) {
+	respObj := &model.ResponseLogin{
+		ResponseCode: "00",
+		ResponseDesc: "Login Success",
+	}
 	loginBean := new(model.LoginBean)
 	decoder := json.NewDecoder(request.Body)
 	err := decoder.Decode(&loginBean)
@@ -19,10 +28,30 @@ func PostLogin(w http.ResponseWriter, request *http.Request) {
 		fmt.Printf("Cannot decode json form with error %s", err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 	}
-	respObj := &model.ResponseLogin{
-		ResponseCode: "00",
-		ResponseDesc: "Login Success",
+
+	jsonvalid, err := os.Open("valid-user.json")
+	if err != nil {
+		log.Printf("Cannot open file name valid-user.json with error: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		respObj.ResponseCode = "10"
+		respObj.ResponseDesc = err.Error()
+		_ = json.NewEncoder(w).Encode(respObj)
+		return
 	}
+
+	defer jsonvalid.Close()
+	byteValue, _ := ioutil.ReadAll(jsonvalid)
+	var result map[string]interface{}
+	_ = json.Unmarshal([]byte(byteValue), &result)
+
+	if loginBean.Username != result["username"] || loginBean.Password != result["password"] {
+		w.WriteHeader(http.StatusBadRequest)
+		respObj.ResponseCode = "10"
+		respObj.ResponseDesc = "Username or Password is invalid."
+		_ = json.NewEncoder(w).Encode(respObj)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(respObj)
 }
